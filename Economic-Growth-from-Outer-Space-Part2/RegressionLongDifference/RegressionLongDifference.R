41c07a322aaaba0267eeadb146f09dbbe30da3b0
zhang ran <44030648+zhangran95@users.noreply.github.com>
2019-03-13 12:27:24 +0100

    Update RegressionLongDifference.R

--- Economic-Growth-from-Outer-Space-Part2/RegressionLongDifference/RegressionLongDifference.R
+++ Economic-Growth-from-Outer-Space-Part2/RegressionLongDifference/RegressionLongDifference.R
@@ -16,20 +16,17 @@ wide = wide[complete.cases(wide) &
 fmla.long = realGDP9205 ~ lightarea9205
 model = list()
 model[['longdif.9205.full']] = lm(fmla.long, data = wide)
-model[['longdif.9205.re']] = coeftest(model[["longdif.9205.full"]], 
-                                     vcov = vcovHC(model[["longdif.9205.full"]],
-                                                   "HC1"))
+model[['longdif.9205.re']]   = coeftest(model[["longdif.9205.full"]], 
+                                        vcov = vcovHC(model[["longdif.9205.full"]], "HC1"))
 summary(model[['longdif.9205.full']])
 
 # explore model 
-model[['longdif.9213']] = lm(realGDP9213 ~ lightarea9213, data = wide)
+model[['longdif.9213']]   = lm(realGDP9213 ~ lightarea9213, data = wide)
 model[['longdif.9213re']] = coeftest(model[["longdif.9213"]], 
-                                      vcov = vcovHC(model[["longdif.9213"]],
-                                                    "HC1"))
-model[['longdif.0513']] = lm(realGDP0513 ~ lightarea0513, data = wide)
-model[['long.0513re']] = coeftest(model[["longdif.0513"]], 
-                                      vcov = vcovHC(model[["longdif.0513"]],
-                                                    "HC1"))
+                                     vcov = vcovHC(model[["longdif.9213"]], "HC1"))
+model[['longdif.0513']]   = lm(realGDP0513 ~ lightarea0513, data = wide)
+model[['long.0513re']]    = coeftest(model[["longdif.0513"]], 
+                                     vcov = vcovHC(model[["longdif.0513"]], "HC1"))
 ##################################################################
 #table code with stargazer
 stargazer(model[['longdif.9205.full']], 
